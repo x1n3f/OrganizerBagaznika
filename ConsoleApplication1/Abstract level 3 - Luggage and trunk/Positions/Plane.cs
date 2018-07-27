2fab7a9df31c73040ddb3aa904f5f6b68febd763
x1n3f <nurzynski@gmail.com>
2018-07-27 12:25:04 +0200

    Created classes to store position of luggage in the trunk. Some work still required

---
+++ ConsoleApplication1/Abstract level 3 - Luggage and trunk/Positions/Plane.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerBagaznika
+{
+    class Plane
+    {
+        private Point A;
+        private Point B;
+        private Point C;
+        private Point D;
+
+        public Plane(Point A, Point B, Point C, Point D)
+        {
+            this.A = new Point(A.X, A.Y, A.Z);
+            this.B = new Point(B.X, B.Y, B.Z);
+            this.C = new Point(C.X, C.Y, C.Z);
+            this.D = new Point(D.X, D.Y, D.Z);
+        }
+
+        public Plane(Point startingPoint, int width, int height, int depth)
+        {
+            this.A = new Point(startingPoint.X, startingPoint.Y, depth);
+            this.B = new Point(this.A.X + width, this.A.Y, depth);
+            this.C = new Point(this.B.X, this.B.Y + height, depth);
+            this.D = new Point(this.A.X, this.C.Y, depth);
+        }
+    }
+}
